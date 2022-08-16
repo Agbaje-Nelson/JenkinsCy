955a1d3db8d251473ff24ec05c4ae026442a28b0
mac <mac@macs-MacBook-Pro.local>
2022-08-16 13:00:53 +0100

    Added Mochawesome report

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require("cypress");
+const Mochawesome = require("mochawesome");
 
 module.exports = defineConfig({
+  "reporter":"Mochawesome",
+  "reporterOptions":{
+    "reportDir":"cypress/reports",
+    "overwrite":false,
+    "html":false,
+    "json":true,
+    "timeStamp":"mmddyyyy_HHMMss"
+    },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
